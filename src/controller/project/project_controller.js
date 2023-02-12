0e1cd08e4c73a85435749a3416e76ab7e3c09ca3
Khanh Tran <khanhsbtqk@gmail.com>
2023-02-12 14:12:08 +0700

    hot fix

--- src/controller/project/project_controller.js
+++ src/controller/project/project_controller.js
@@ -221,7 +221,7 @@ const projectController = {
         return res.status(400).send({ msg: "This project doesn't exist" });
       }
 
-      res.status(200).send(project);
+      res.status(200).send({ project: project });
     } catch (err) {
       res.status(400).send({ msg: err.message });
     }
