2da7b842edbe377faf1623aeb90fae0c3a81bb65
Khanh Tran <khanhsbtqk@gmail.com>
2023-03-28 18:14:15 +0700

    [FIX] - help boost the npm i cmd

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,6 @@
 echo "Starting the server"
 
+sudo npm set progress=false
+sudo npm config set registry http://registry.npmjs.org/
 sudo npm install --prefix /home/ubuntu/Code/coffee-supply-chain
 sudo systemctl restart cf.service
