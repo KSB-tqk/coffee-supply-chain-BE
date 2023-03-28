b6dc4c66aedd5ace216863d580f03aa44c7f1b9e
Khanh Tran <khanhsbtqk@gmail.com>
2023-03-28 17:10:33 +0700

    [FIX] - remove npm run dev from start server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,4 @@
 echo "Starting the server"
 
 sudo npm install --prefix /home/ubuntu/Code/coffee-supply-chain
-npm run dev --prefix /home/ubuntu/Code/coffee-supply-chain
 sudo systemctl restart cf.service
