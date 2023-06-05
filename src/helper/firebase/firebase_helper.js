fdcc3013c2b34e4e504efe9dec4c083cd489e973
Khanh Tran <khanhsbtqk@gmail.com>
2023-06-05 14:58:32 +0700

    [Hot-fix] - google cloud token leak

--- src/helper/firebase/firebase_helper.js
+++ src/helper/firebase/firebase_helper.js
@@ -1,8 +1,34 @@
 import admin from "firebase-admin";
-import serviceJson from "./coffee-supply-chain-2c30f-firebase-adminsdk-v5jv0-726d131bf0.json" assert { type: "json" };
+import dotenv from "dotenv";
+import path from "path";
+import { dirname } from "path";
+
+dotenv.config({ path: path.resolve(dirname + "/dev.env") });
+
+const obj = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_email,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain,
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceJson),
+  credential: admin.credential.cert(obj),
 });
+
+function convertToEnv(object) {
+  let envFile = "";
+  for (const key of Object.keys(object)) {
+    envFile += `${key}=${object[key]}\n`;
+  }
+  return envFile;
+}
 
 export default admin;
