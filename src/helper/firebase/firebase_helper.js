07ed45c18b88152a066a576cff0ee5d1b7b8a8ca
Khanh Tran <khanhsbtqk@gmail.com>
2023-06-08 10:36:22 +0700

    [Fix] - bug firebase config json account

--- src/helper/firebase/firebase_helper.js
+++ src/helper/firebase/firebase_helper.js
@@ -5,12 +5,10 @@ import { dirname } from "path";
 
 dotenv.config({ path: path.resolve(dirname + "/dev.env") });
 
-import firebaseConfig from "./coffee-supply-chain-2c30f-firebase-adminsdk-v5jv0-7e64f6c9d7.json" assert { type: "json" };
+import firebaseConfig from "./firebase-account-config.json" assert { type: "json" };
 
 admin.initializeApp({
   credential: admin.credential.cert(firebaseConfig),
 });
 
 export default admin;
-
-//9bea460f1923ce5b055a3b35cc44b0f7b7bdc446
